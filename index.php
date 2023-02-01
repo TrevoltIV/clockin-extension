dfa685398c4982531cfca49b302fc06b01f1affc
Karsten Koerner <kgk1999@gmail.com>
2023-01-31 23:36:24 -0500

    hello world

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// I see you are checking out my code, nice. Enjoy your stay, fellow programmer.
+
+$dummy_backend_for_now = 'This backend is not yet implemented. It purely exists so Heroku can deploy the app.';
+if ($dummy_backend_for_now === 'Backend is implemented') {
+    echo "I'M A GIRAFFE";
+    exit;
+} else {
+    exit;
+}
+
+?>
